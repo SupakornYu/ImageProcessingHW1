b1b963dff503687038882e22d332f547435bd184
supakorn <supakorn_pump@hotmail.com>
2015-02-24 16:08:40 +0700

    -scale and central moment is done

--- img_main.py
+++ img_main.py
@@ -1,7 +1,8 @@
 import wx
-import ImageTk
+"""import ImageTk"""
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 class Panel1(wx.Panel):
     """class Panel1 creates a panel with an image on it, inherits wx.Panel"""
@@ -40,7 +41,7 @@ class ImageLib:
         pgmSize = pgmComment_eachline.split()
         pgmGreyscale = file.readline().split()
         pgmDataList = []
-        htg = np.zeros((256))
+        htg = np.zeros((256),dtype=np.int32)
         np.set_printoptions(suppress=True)
         for j in range(int(pgmSize[1])):
             pgmDataX = []
@@ -51,8 +52,11 @@ class ImageLib:
                 htg[chrToInt] = htg[chrToInt]+1
             pgmDataList.append(pgmDataX)
         file.close()
-        pgmData = np.asarray(pgmDataList)
+        pgmData = np.asarray(pgmDataList,dtype=np.int32)
         return pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg
+        #pgmData is data pixel that i get from pgm file under grey level value(numpy array).
+        #pgmSize contain width and height of pixel(list).
+        #htg is a histogram of image (numpy array).
 
     def plotHistogramFromArray(self,histogram_arr):
         index = np.arange(256)
@@ -69,52 +73,80 @@ class ImageLib:
 
     def countingObject(self,histogram,threshold_object):
         countObject = 0
-        for itemHis in histogram:
-            if itemHis >= threshold_object:
+        countObjectGreyLevel = []
+        for i in range(histogram.size):
+            if histogram[i] >= threshold_object:
                 countObject += 1
-        return countObject-1 # minus 1 for backgroud
+                countObjectGreyLevel.append(i)
+        countObjectGreyLevel.remove(max(countObjectGreyLevel))
+        return countObject-1,countObjectGreyLevel # minus 1 for backgroud
 
-"""
-file = open("scaled_shapes.pgm", "rb")
-pgmVer = file.readline().split()
-pgmComment = []
-while True:
-	pgmComment_eachline = file.readline()
-	if(pgmComment_eachline[0]=="#"):
-		pgmComment.append(pgmComment_eachline)
-	else:
-		break
-pgmSize = pgmComment_eachline.split()
-pgmGreyscale = file.readline().split()
+    def pqMoment(self,p,q,pqmData,pgmSize,greyLevel,greyLevelSelected):
+        moment = 0
+        pgmDataMoment = np.zeros((int(pgmSize[1]),int(pgmSize[0])), dtype=np.int32)
+        for i in range(int(pgmSize[1])):
+            for j in range(int(pgmSize[0])):
+                if pgmData[i][j] == greyLevelSelected:
+                    pgmDataMoment[i][j] = 1
+                else:
+                    pgmDataMoment[i][j] = 0
+                moment += ((math.pow(j,p))*((math.pow(i,q))*pgmDataMoment[i][j]))
+        #ImageLib.buildPGMFile(self,"testmoment",pgmSize[0],pgmSize[1],greyLevel,pgmDataMoment)
+        return moment,pgmDataMoment
 
-pgmDataList = []
-print pgmVer
-print pgmComment
-print pgmSize
-print pgmGreyscale
-threshold_object = 1000
-htg = np.zeros((256))
-for j in range(int(pgmSize[1])):
-    pgmDataX = []
-    for i in range(int(pgmSize[0])):
-        byte = file.read(1)
-        chrToInt = ord(byte)
-        pgmDataX.append(chrToInt)
-        htg[chrToInt] = htg[chrToInt]+1
-    pgmDataList.append(pgmDataX)
-file.close()
-pgmData = np.asarray(pgmDataList)
-np.set_printoptions(suppress=True)
-print pgmData
-print htg
+    def centralMoment(self,p,q,pgmData,pgmSize,greyLevel,greyLevelSelected):
+        centralMoment = 0
+        moment1,pgmDataMoment = ImageLib.pqMoment(self,1,0,pgmData,pgmSize,greyLevel,greyLevelSelected)
+        moment2,pgmDataMoment = ImageLib.pqMoment(self,0,1,pgmData,pgmSize,greyLevel,greyLevelSelected)
+        moment3,pgmDataMoment = ImageLib.pqMoment(self,0,0,pgmData,pgmSize,greyLevel,greyLevelSelected)
+        xCoor = moment1/moment3
+        yCoor = moment2/moment3
+        for i in range(int(pgmSize[1])):
+            for j in range(int(pgmSize[0])):
+                centralMoment += ((math.pow((j-xCoor),p))*((math.pow(i-yCoor,q))*pgmDataMoment[i][j]))
+        return centralMoment
 
-"""
+    def scaleInvariantMoment(self,p,q,pgmData,pgmSize,greyLevel,greyLevelSelected):
+        scaleInvariantMoment = 0
+        centralMomentPQ = ImageLib.centralMoment(self,p,q,pgmData,pgmSize,greyLevel,greyLevelSelected)
+        centralMoment00 = ImageLib.centralMoment(self,0,0,pgmData,pgmSize,greyLevel,greyLevelSelected)
+        scaleInvariantMoment = centralMomentPQ/(math.pow(centralMoment00,(1+((p+q)/2))))
+        return scaleInvariantMoment
+
+    def buildPGMFile(self,fileName,width,height,greyLevel,pgmData):
+        f = open(str(fileName)+".pgm","wb")
+        f.write("P5\n");
+        f.write("# "+str(fileName)+"\n");
+        f.write(str(width)+" "+str(height)+"\n"+str(greyLevel[0])+"\n");
+        for i in range(int(height)):
+            for j in range(int(width)):
+                f.write(chr(pgmData[i][j]));
+        f.close()
+
 
 myLib = ImageLib()
 pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg = myLib.readPGMImage('scaled_shapes.pgm')
+#myLib.buildPGMFile("test",pgmSize[0],pgmSize[1],pgmGreyscale,pgmData)
+print htg
+#monent,pgmDataMoment =  myLib.pqMoment(1,1,pgmData,pgmSize,pgmGreyscale,255)
+print "object : "+ str(myLib.countingObject(htg,1000))
+
+"""
+#1.2
+print myLib.centralMoment(2,0,pgmData,pgmSize,pgmGreyscale,80)
+print myLib.scaleInvariantMoment(2,0,pgmData,pgmSize,pgmGreyscale,80)+myLib.scaleInvariantMoment(0,2,pgmData,pgmSize,pgmGreyscale,80)
+"""
+
+
+"""
+pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg = myLib.readPGMImage('scaled_shapes.pgm')
+#print ImageLib().pqMoment(1,1,pgmData,pgmSize)
+print pgmData
+print pgmSize
 print htg
 myLib.plotHistogramFromArray(htg)
-print "object : "+ str(myLib.countingObject(htg,1000))
+
+"""
 
 app = wx.App()
 # create a window/frame, no parent, -1 is default ID
